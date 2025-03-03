e3a35deb96e6bc8220056e5deb07dac2257ebf68
mdasifalijihat <mdasifalijihat@gmail.com>
2025-03-04 00:50:58 +0600

    first commit

---
+++ recap.js
@@ -0,0 +1,28 @@
+/**
+ * 1. how to use ja with html and css
+ * 2. get element or elements from dom
+ * 2-1: getElementById
+ * 2-2: getRlementsByClassName
+ * 2-3: queryselector
+ * 2-4: queryselectorAll
+ * 
+ * 
+ * 3. dynamically and elementto the dom: document.createElement
+ * 3-5: set value to the innerHtml
+ * 
+ * 4. get and set value from/to a dom element
+ * 4-1: use innertext (h1, p, h5, div, span, strong)
+ * 4-2: use value (input, textArea)
+ * 
+ * 5. addd event lister (event handler )
+ * 5-1: click, focus, blur, onchange, keyup
+ * 5-2: addEbentlistener onclick = "myHandler()"
+ * 
+ * 6. use function to reduce duplicae code
+ * 
+ * 7. event delegate 
+ * 
+ * */ 
+
+document.querySelectorAll('.users-container .disable h3');
+document.querySelectorAll('.my-users li')
---
+++ valadation.js
@@ -0,0 +1,13 @@
+const number = '23';
+
+// string, number, boolean 
+if(typeof number === 'number'){
+    console.log('value is a number ')
+}else{
+    console.log('value is a not number ')
+}
+
+const numbers = [3, 12, 78, 25];
+const sutudnt = {name: 'Rohan Rafsan', job: 'Chamili Katun '};
+console.log(typeof number);
+console.log(typeof sutudnt)
